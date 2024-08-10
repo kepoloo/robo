4bc3e4da8a104384cc528065e5c62d70f3a99969
Haiki <arlozayden2@gmail.com>
2024-08-11 03:45:07 +0700

    Update banall.py

--- plugins/banall.py
+++ plugins/banall.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import ChatBannedRights
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon import events
-from AvishaRobot import telethn as tbot, DEV_USERS
+from VIPMUSIC import telethn as tbot, DEV_USERS
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
